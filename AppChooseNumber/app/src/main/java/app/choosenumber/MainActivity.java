0df4dfc94fe249ef472ab219eed0074bd1917347
ana204 <anagomes.lucia@hotmail.com>
2022-01-11 17:15:24 -0300

    Random Number

--- AppChooseNumber/app/src/main/java/app/choosenumber/MainActivity.java
+++ AppChooseNumber/app/src/main/java/app/choosenumber/MainActivity.java
@@ -2,13 +2,40 @@ package app.choosenumber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnPlay;
+    TextView SelectionNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        btnPlay = findViewById(R.id.btnPlay);
+        SelectionNumber = findViewById(R.id.SelectionNumber);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random random = new Random();
+
+                int randomNumbers = random.nextInt(61);
+                SelectionNumber.setText("Número selecionado: " + randomNumbers);
+
+            }
+        });
     }
 }
