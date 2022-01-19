dc302f95f06c66c861247560474bf254583cb0a2
ana204 <anagomes.lucia@hotmail.com>
2022-01-19 16:06:08 -0300

    clicando no item das imagens

--- AppGame/app/src/main/java/app/pedra/papeltesoura/MainActivity.java
+++ AppGame/app/src/main/java/app/pedra/papeltesoura/MainActivity.java
@@ -3,12 +3,56 @@ package app.pedra.papeltesoura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imgPedra, imgPapel, imgtesoura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgPedra = findViewById(R.id.imgPedra);
+        imgPapel = findViewById(R.id.imgPapel);
+        imgtesoura = findViewById(R.id.imgtesoura);
+
+        SelecionadoPedra();
+        SelecionadoPapel();
+        SelecionadoTesoura();
+    }
+
+    public void opcaoSelecionada(String opcaoSelecionada){
+        System.out.println(opcaoSelecionada);
+    }
+
+    public void SelecionadoPedra(){
+        imgPedra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                opcaoSelecionada("CLICOU NA PEDRA");
+            }
+        });
+    }
+
+    public void SelecionadoPapel(){
+        imgPapel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opcaoSelecionada("CLICOU NO PAPEL");
+            }
+        });
+    }
+
+    public void SelecionadoTesoura(){
+        imgtesoura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opcaoSelecionada("CLICOU NA TESOURA");
+            }
+        });
     }
 }
