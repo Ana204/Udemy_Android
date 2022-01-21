dcab837cc7e23dce3ce5c023cf98134bed83ea9b
ana204 <anagomes.lucia@hotmail.com>
2022-01-21 16:03:48 -0300

    getText

--- ComponentesBasicos/app/src/main/java/app/pedra/componentes/MainActivity.java
+++ ComponentesBasicos/app/src/main/java/app/pedra/componentes/MainActivity.java
@@ -3,13 +3,17 @@ package app.pedra.componentes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText edtNome, edtEmail;
+    EditText edtNome;
+    TextInputEditText edtEmail;
     Button ButtonEnviar;
     TextView edtResultado;
 
@@ -17,5 +21,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        edtNome = findViewById(R.id.edtNome);
+        edtEmail = findViewById(R.id.edtEmail);
+        ButtonEnviar = findViewById(R.id.ButtonEnviar);
+        edtResultado = findViewById(R.id.edtResultado);
+
+
+        ButtonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String nome = edtNome.getText().toString();
+                String email = edtEmail.getText().toString();
+
+                edtResultado.setText("Nome: "+ nome + "Email: " + email);
+            }
+        });
     }
 }
