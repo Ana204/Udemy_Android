6372f0d65b303359cf3976ef288e5a1eee0dd019
ana204 <anagomes.lucia@hotmail.com>
2022-01-21 12:48:05 -0300

    posicionamento de componentes

--- ComponentesBasicos/app/src/main/java/app/pedra/componentes/MainActivity.java
+++ ComponentesBasicos/app/src/main/java/app/pedra/componentes/MainActivity.java
@@ -3,8 +3,15 @@ package app.pedra.componentes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtNome, edtEmail;
+    Button ButtonEnviar;
+    TextView edtResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
