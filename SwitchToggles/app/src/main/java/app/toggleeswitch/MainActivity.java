2709c76b724bfc9879eae80bc82d822d40269eeb
ana204 <anagomes.lucia@hotmail.com>
2022-01-31 10:32:24 -0300

    MonitorarSwitch()

--- SwitchToggles/app/src/main/java/app/toggleeswitch/MainActivity.java
+++ SwitchToggles/app/src/main/java/app/toggleeswitch/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -27,11 +28,39 @@ import android.widget.ToggleButton;
         btnEnviar = findViewById(R.id.btnEnviar);
         txtResultado = findViewById(R.id.txtResultado);
 
+        MonitorarSwitch();
+
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+/*
+                if (SwitchLembrarSenha.isChecked()){
+                    txtResultado.setText("Switch Ligado");
+                }else {
+                    txtResultado.setText("Switch desligado");
+                }*/
+
+/*                if (ToggleButton.isChecked()){
+                    txtResultado.setText("Toggle Ligado");
+                }else {
+                    txtResultado.setText("Toggle desligado");
+                }*/
             }
         });
     }
-}
+
+     private void MonitorarSwitch() {
+
+        SwitchLembrarSenha.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+
+                if (checked){
+                    txtResultado.setText("Switch Ligado");
+                }else {
+                    txtResultado.setText("Switch desligado");
+                }
+            }
+        });
+     }
+ }
