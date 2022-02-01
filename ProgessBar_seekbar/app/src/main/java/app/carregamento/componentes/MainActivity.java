5c62c3a3f72c24c48b865c2274490f587f5c2159
ana204 <anagomes.lucia@hotmail.com>
2022-02-01 09:47:56 -0300

    seekbar

--- ProgessBar_seekbar/app/src/main/java/app/carregamento/componentes/MainActivity.java
+++ ProgessBar_seekbar/app/src/main/java/app/carregamento/componentes/MainActivity.java
@@ -6,11 +6,15 @@ import androidx.appcompat.widget.AppCompatButton;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    AppCompatButton buttonCarregar;
+    AppCompatButton buttonCarregar,buttonRecuperar;
     ProgressBar progressBarCircular, progressBarHorizontal;
+    SeekBar seekbarEscala;
+    TextView resultado;
     int progress = 0;
 
     @Override
@@ -21,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
         buttonCarregar = findViewById(R.id.buttonCarregar);
         progressBarCircular = findViewById(R.id.progressBarCircular);
         progressBarHorizontal = findViewById(R.id.progressBarHorizontal);
+        buttonRecuperar = findViewById(R.id.buttonRecuperar);
+        seekbarEscala = findViewById(R.id.seekbarEscala);
+        resultado = findViewById(R.id.resultado);
 
         progressBarCircular.setVisibility(View.GONE);
 
@@ -36,5 +43,31 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        seekbarEscala.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+            resultado.setText("RESULTADO: " + progress + "/" + seekBar.getMax());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        buttonRecuperar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultado.setText("ESCOLHIDO " + seekbarEscala.getProgress());
+            }
+        });
     }
+
+
 }
