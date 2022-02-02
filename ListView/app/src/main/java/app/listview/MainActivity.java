68d70455acdf31679b34ac4f144c06c5b0fc6dac
ana204 <anagomes.lucia@hotmail.com>
2022-02-02 15:20:55 -0300

    lista de itens

--- ListView/app/src/main/java/app/listview/MainActivity.java
+++ ListView/app/src/main/java/app/listview/MainActivity.java
@@ -3,12 +3,24 @@ package app.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listLocais;
+    String[] itens = {
+            "Angra dos Reis", "Caldas Novas",
+            "Campos do Jordão", "Costa do Sauípe",
+            "Ilhéus", "Porto Seguro", "Tiradentes",
+            "Praga", "Santiago", "Zurique", "Caribe",
+            "Buenos Aires", "Budapeste", "Cancún", "Aruba"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listLocais = findViewById(R.id.listLocais);
     }
 }
