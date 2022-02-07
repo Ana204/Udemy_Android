3970e8b5a87b9f393a9576ab6809a5e74a9a5688
ana204 <anagomes.lucia@hotmail.com>
2022-02-07 16:17:02 -0300

    lista de filmes

--- RecyclerView/app/src/main/java/app/recyclerview/view/MainActivity.java
+++ RecyclerView/app/src/main/java/app/recyclerview/view/MainActivity.java
@@ -6,12 +6,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import app.recyclerview.R;
 import app.recyclerview.adapter.Adapter;
+import app.recyclerview.model.Filme;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    private List<Filme> listaFilmes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +26,34 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerView);
         Adapter adapter = new Adapter();
 
+        //lista de filmes
+        criarFilmes();
+
         //configurar recyclerView
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(adapter);
+    }
+
+    public void criarFilmes(){
+        Filme filme = new Filme("Homem Aranha - De volta ao lar", "Aventura", "2021");
+        listaFilmes.add(filme);
+
+        filme = new Filme("Mulher Maravilha", "Fantasia", "2017");
+        listaFilmes.add(filme);
+
+        filme = new Filme("Liga da Justiça", "Ficção", "2017");
+        listaFilmes.add(filme);
+
+        filme = new Filme("Capitão América", "Aventura/Ficção", "2016");
+        listaFilmes.add(filme);
+
+        filme = new Filme("A Múmia", "Terror", "2017");
+        listaFilmes.add(filme);
+
+        filme = new Filme("A bela ea Fera", "Romance", "2016");
+        listaFilmes.add(filme);
+
     }
 }
