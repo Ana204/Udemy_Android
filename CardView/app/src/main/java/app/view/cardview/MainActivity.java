1e09f6011249c1fc74e58eca69837642f04a9782
ana204 <anagomes.lucia@hotmail.com>
2022-02-10 15:48:49 -0300

    recyclerViewPostagem id

--- CardView/app/src/main/java/app/view/cardview/MainActivity.java
+++ CardView/app/src/main/java/app/view/cardview/MainActivity.java
@@ -1,14 +1,20 @@
 package app.view.cardview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerViewPostagem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewPostagem = findViewById(R.id.recyclerViewPostagem);
+
     }
 }
