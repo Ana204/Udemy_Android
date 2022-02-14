0211bb4b4bd1cf6077163144b6b700714abde533
ana204 <anagomes.lucia@hotmail.com>
2022-02-14 12:33:50 -0300

    Layout

--- CardView/app/src/main/java/app/view/cardview/adapter/PostagemAdapter.java
+++ CardView/app/src/main/java/app/view/cardview/adapter/PostagemAdapter.java
@@ -1,6 +1,5 @@
 package app.view.cardview.adapter;
 
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,27 +8,18 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import app.view.cardview.R;
-
 public class PostagemAdapter extends RecyclerView.Adapter<PostagemAdapter.MyViewHolder> {
 
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-       // View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.postagem_list, parent, false);
-
-       // return new MyViewHolder(itemLista);
         return null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
 
-        holder.nome.setText("Ana Lucia Gomes");
-        holder.textPostagem.setText("#tbt Viagem legall !!");
-        holder.imgPostagem.setImageResource(R.drawable.paisagem3);
     }
 
     @Override
