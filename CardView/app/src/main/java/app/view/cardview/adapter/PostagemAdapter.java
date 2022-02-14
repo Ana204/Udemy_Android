7985b8d39177fbb303d4e83062f89bedf036b869
ana204 <anagomes.lucia@hotmail.com>
2022-02-14 16:21:14 -0300

    onBindViewHolder

--- CardView/app/src/main/java/app/view/cardview/adapter/PostagemAdapter.java
+++ CardView/app/src/main/java/app/view/cardview/adapter/PostagemAdapter.java
@@ -1,12 +1,16 @@
 package app.view.cardview.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.dynamicanimation.animation.DynamicAnimation;
 import androidx.recyclerview.widget.RecyclerView;
+
+import app.view.cardview.R;
 
 public class PostagemAdapter extends RecyclerView.Adapter<PostagemAdapter.MyViewHolder> {
 
@@ -14,17 +18,21 @@ public class PostagemAdapter extends RecyclerView.Adapter<PostagemAdapter.MyView
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.postagem_lista, parent, false);
+        return new MyViewHolder(itemLista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        holder.nome.setText("ANA LUCIA GOMES");
+        holder.textPostagem.setText("#tbt VIAGEM INCRIIIVEL !!");
+        holder.imgPostagem.setImageResource(R.drawable.paisagem2);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 6;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
@@ -35,9 +43,9 @@ public class PostagemAdapter extends RecyclerView.Adapter<PostagemAdapter.MyView
         public MyViewHolder(View itemView) {
             super(itemView);
 
-/*            nome = itemView.findViewById(R.id.nome);
+            nome = itemView.findViewById(R.id.nome);
             textPostagem = itemView.findViewById(R.id.textPostagem);
-            imgPostagem = itemView.findViewById(R.id.imgPostagem);*/
+            imgPostagem = itemView.findViewById(R.id.imgPostagem);
         }
     }
 }
