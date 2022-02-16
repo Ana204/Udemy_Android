e5cf28ef1303ff385dd26ffb50f9d926e79028fc
ana204 <anagomes.lucia@hotmail.com>
2022-02-16 15:26:53 -0300

    objeto buttonVoltar

--- CaraOuCoroa/app/src/main/java/app/mobile/caraoucoroa/ResultadoActivity.java
+++ CaraOuCoroa/app/src/main/java/app/mobile/caraoucoroa/ResultadoActivity.java
@@ -1,14 +1,31 @@
 package app.mobile.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ResultadoActivity extends AppCompatActivity {
+
+    AppCompatButton buttonVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        getSupportActionBar().hide();
+
+        buttonVoltar = findViewById(R.id.buttonVoltar);
+
+        buttonVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ResultadoActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
