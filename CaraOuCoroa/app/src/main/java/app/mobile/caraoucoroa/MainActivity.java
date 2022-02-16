91d111ce50d5b2bca18c24bc2be8bb3d06f613d6
ana204 <anagomes.lucia@hotmail.com>
2022-02-16 15:15:20 -0300

    objeto buttonJogar e colors

--- CaraOuCoroa/app/src/main/java/app/mobile/caraoucoroa/MainActivity.java
+++ CaraOuCoroa/app/src/main/java/app/mobile/caraoucoroa/MainActivity.java
@@ -1,14 +1,33 @@
 package app.mobile.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    AppCompatButton buttonJogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        buttonJogar = findViewById(R.id.buttonJogar);
+
+        buttonJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, ResultadoActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
