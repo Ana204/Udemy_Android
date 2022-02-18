c97adeb9546a58c78dde71e3b1860a6a3d39b9a5
ana204 <anagomes.lucia@hotmail.com>
2022-02-18 11:16:01 -0300

    Configurar obj para fragmento

--- Fragments/app/src/main/java/app/mobile/fragments/activity/MainActivity.java
+++ Fragments/app/src/main/java/app/mobile/fragments/activity/MainActivity.java
@@ -1,12 +1,22 @@
 package app.mobile.fragments.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import app.mobile.fragments.R;
+import app.mobile.fragments.fragments.ContatoFragment;
+import app.mobile.fragments.fragments.ConversasFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    AppCompatButton buttonConversas, buttonContato;
+    ConversasFragment conversasFragment;
+    ContatoFragment contatoFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +25,40 @@ public class MainActivity extends AppCompatActivity {
 
         //remover a sobra do actionBar
         getSupportActionBar().setElevation(0);
+
+        buttonConversas = findViewById(R.id.buttonConversas);
+        buttonContato = findViewById(R.id.buttonContato);
+
+
+        conversasFragment = new ConversasFragment();
+
+        //Configurar obj para fragmento
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.FrameLayout, conversasFragment);
+        transaction.commit();
+
+        buttonConversas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                conversasFragment = new ConversasFragment();
+
+                //Configurar obj para fragmento
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.FrameLayout, conversasFragment);
+                transaction.commit();
+            }
+        });
+
+        buttonContato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contatoFragment = new ContatoFragment();
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.FrameLayout, contatoFragment);
+                transaction.commit();
+            }
+        });
     }
 }
--- Fragments/app/src/main/java/app/mobile/fragments/fragments/ContatoFragment.java
+++ Fragments/app/src/main/java/app/mobile/fragments/fragments/ContatoFragment.java
@@ -7,11 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import app.mobile.fragments.R;
 
 
 public class ContatoFragment extends Fragment {
+
+    TextView txtContato;
 
     public ContatoFragment() {
         // Required empty public constructor
@@ -21,6 +24,11 @@ public class ContatoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contato, container, false);
+        View view = inflater.inflate(R.layout.fragment_contato, container, false);
+
+        txtContato = view.findViewById(R.id.txtContato);
+        txtContato.setText(R.string.contato);
+
+        return view;
     }
 }
