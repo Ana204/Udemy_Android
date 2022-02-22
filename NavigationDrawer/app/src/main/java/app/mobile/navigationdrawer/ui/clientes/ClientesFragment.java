b9751e0583626ab760ac9e42945e7b3e4bf5e583
ana204 <anagomes.lucia@hotmail.com>
2022-02-22 12:43:43 -0300

    fragments

---
+++ NavigationDrawer/app/src/main/java/app/mobile/navigationdrawer/ui/clientes/ClientesFragment.java
@@ -0,0 +1,31 @@
+package app.mobile.navigationdrawer.ui.clientes;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.mobile.navigationdrawer.R;
+
+public class ClientesFragment extends Fragment {
+
+    public ClientesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_clientes, container, false);
+    }
+}
