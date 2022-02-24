9df82434bd67dea2ec3467b708ab1a647c3090ce
ana204 <anagomes.lucia@hotmail.com>
2022-02-24 16:23:49 -0300

    declaraçao de objs

--- MediaPlayer/app/src/main/java/app/mobile/mediaplayer/MainActivity.java
+++ MediaPlayer/app/src/main/java/app/mobile/mediaplayer/MainActivity.java
@@ -1,14 +1,25 @@
 package app.mobile.mediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekbarVolume;
+    AppCompatButton buttonPlay, buttonPause, buttonStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekbarVolume = findViewById(R.id.seekbarVolume);
+        buttonPlay = findViewById(R.id.buttonPlay);
+        buttonPause = findViewById(R.id.buttonPause);
+        buttonStop = findViewById(R.id.buttonStop);
+
     }
 }
