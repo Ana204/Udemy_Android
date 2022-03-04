4dd50aac3b3e348feff3dbd66d67e8feb09b02f2
ana204 <anagomes.lucia@hotmail.com>
2022-03-04 10:48:11 -0300

    declaração de objs

--- AbasSmartTableLayout/app/src/main/java/app/mobile/smarttablelayout/MainActivity.java
+++ AbasSmartTableLayout/app/src/main/java/app/mobile/smarttablelayout/MainActivity.java
@@ -1,14 +1,23 @@
 package app.mobile.smarttablelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    SmartTabLayout ViewPagerTab;
+    ViewPager ViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPagerTab = findViewById(R.id.ViewPagerTab);
+        ViewPager = findViewById(R.id.ViewPager);
     }
 }
