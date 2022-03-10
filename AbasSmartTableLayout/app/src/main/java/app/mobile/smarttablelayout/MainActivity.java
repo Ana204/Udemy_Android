e9053e61317d83f34af34f6938d6f6412eb27484
ana204 <anagomes.lucia@hotmail.com>
2022-03-10 11:21:52 -0300

    Configurar adapter para abas

--- AbasSmartTableLayout/app/src/main/java/app/mobile/smarttablelayout/MainActivity.java
+++ AbasSmartTableLayout/app/src/main/java/app/mobile/smarttablelayout/MainActivity.java
@@ -9,6 +9,10 @@ import com.ogaclejapan.smarttablayout.SmartTabLayout;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
+import app.mobile.smarttablelayout.Fragments.EmAltaFragment;
+import app.mobile.smarttablelayout.Fragments.HomeFragment;
+import app.mobile.smarttablelayout.Fragments.InscricoesFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     SmartTabLayout ViewPagerTab;
@@ -26,8 +30,13 @@ public class MainActivity extends AppCompatActivity {
         FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
                 getSupportFragmentManager(),
                 FragmentPagerItems.with(this)
-                       // .add("Home",)
+                        .add("Home", HomeFragment.class)
+                        .add("Inscrições", InscricoesFragment.class)
+                        .add("Em Alta", EmAltaFragment.class)
                 .create()
         );
+
+        ViewPager.setAdapter(adapter);
+        ViewPagerTab.setViewPager(ViewPager);
     }
 }
