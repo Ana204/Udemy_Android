1750453d0dbe0d1cd1cd553b679c38973c4f92f5
ana204 <anagomes.lucia@hotmail.com>
2022-03-14 16:35:23 -0300

    fragments

--- AprendaIngles/app/src/main/java/com/example/aprendaingles/MainActivity.java
+++ AprendaIngles/app/src/main/java/com/example/aprendaingles/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.aprendaingles;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    SmartTabLayout ViewPagerTab;
+    ViewPager ViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setElevation(0);
+
+        ViewPagerTab = findViewById(R.id.ViewPagerTab);
+        ViewPager = findViewById(R.id.ViewPager);
     }
 }
