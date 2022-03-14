698238509451b749288e891784a55a91027f6a7d
ana204 <anagomes.lucia@hotmail.com>
2022-03-14 16:44:13 -0300

    Abas

--- AprendaIngles/app/src/main/java/com/example/aprendaingles/MainActivity.java
+++ AprendaIngles/app/src/main/java/com/example/aprendaingles/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.aprendaingles.Fragments.BichosFragment;
+import com.example.aprendaingles.Fragments.NumerosFragment;
+import com.example.aprendaingles.Fragments.VogaisFragment;
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +26,18 @@ public class MainActivity extends AppCompatActivity {
 
         ViewPagerTab = findViewById(R.id.ViewPagerTab);
         ViewPager = findViewById(R.id.ViewPager);
+
+        //Configurar Adapter para abas
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("Bichos", BichosFragment.class)
+                        .add("Números", NumerosFragment.class)
+                        .add("Vogais", VogaisFragment.class)
+                .create()
+        );
+
+        ViewPager.setAdapter(adapter);
+        ViewPagerTab.setViewPager(ViewPager);
     }
 }
