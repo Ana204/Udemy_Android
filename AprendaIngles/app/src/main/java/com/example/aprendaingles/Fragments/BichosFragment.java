10d83bcacfa509f23956e3f07812fc57983661e7
ana204 <anagomes.lucia@hotmail.com>
2022-03-17 10:40:25 -0300

    declaração de objs

--- AprendaIngles/app/src/main/java/com/example/aprendaingles/Fragments/BichosFragment.java
+++ AprendaIngles/app/src/main/java/com/example/aprendaingles/Fragments/BichosFragment.java
@@ -7,15 +7,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.aprendaingles.R;
 
 public class BichosFragment extends Fragment {
 
+    ImageButton imageUm, imgDois, imageTres, imgQuatro, imageCinco, imgSeis;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bichos, container, false);
+        View view =  inflater.inflate(R.layout.fragment_bichos, container, false);
+
+        imageUm = view.findViewById(R.id.imageUm);
+        imgDois = view.findViewById(R.id.imgDois);
+        imageTres = view.findViewById(R.id.imageTres);
+        imgQuatro = view.findViewById(R.id.imgQuatro);
+        imageCinco = view.findViewById(R.id.imageCinco);
+        imgSeis = view.findViewById(R.id.imgSeis);
+
+        return view;
     }
 }
