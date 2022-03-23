54d6364fe77bf0bff290aeb87ed6ddfb2da94119
ana204 <anagomes.lucia@hotmail.com>
2022-03-23 11:42:30 -0300

    obj Declarados

--- AprendaIngles/app/src/main/java/com/example/aprendaingles/Fragments/NumerosFragment.java
+++ AprendaIngles/app/src/main/java/com/example/aprendaingles/Fragments/NumerosFragment.java
@@ -7,16 +7,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.aprendaingles.R;
 
 
-public class NumerosFragment extends Fragment {
+public class NumerosFragment extends Fragment implements View.OnClickListener{
+
+    ImageButton imageUm, imgDois, imageTres, imgQuatro, imageCinco, imgSeis;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_numeros, container, false);
+        View view = inflater.inflate(R.layout.fragment_numeros, container, false);
+
+        imageUm = view.findViewById(R.id.imageUm);
+        imgDois = view.findViewById(R.id.imgDois);
+        imageTres = view.findViewById(R.id.imageTres);
+        imgQuatro = view.findViewById(R.id.imgQuatro);
+        imageCinco = view.findViewById(R.id.imageCinco);
+        imgSeis = view.findViewById(R.id.imgSeis);
+
+       return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
