91b72e9cfed30b5fdb985705af0726c7e6716b04
ana204 <anagomes.lucia@hotmail.com>
2022-03-23 14:36:11 -0300

    obj são declarados

--- AprendaIngles/app/src/main/java/com/example/aprendaingles/Fragments/VogaisFragment.java
+++ AprendaIngles/app/src/main/java/com/example/aprendaingles/Fragments/VogaisFragment.java
@@ -7,15 +7,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.aprendaingles.R;
 
 public class VogaisFragment extends Fragment {
 
+    ImageButton imageUm, imgDois, imageTres, imgQuatro, imageCinco;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vogais, container, false);
+        View view = inflater.inflate(R.layout.fragment_vogais, container, false);
+
+        imageUm = view.findViewById(R.id.imageUm);
+        imgDois = view.findViewById(R.id.imgDois);
+        imageTres = view.findViewById(R.id.imageTres);
+        imgQuatro = view.findViewById(R.id.imgQuatro);
+        imageCinco = view.findViewById(R.id.imageCinco);
+
+        return view;
     }
 }
