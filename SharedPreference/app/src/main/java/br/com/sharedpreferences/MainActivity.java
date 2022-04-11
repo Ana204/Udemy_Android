3755a578919e0a55f42c00e7879253487f141811
ana204 <anagomes.lucia@hotmail.com>
2022-04-11 17:12:00 -0300

    obj

--- SharedPreference/app/src/main/java/br/com/sharedpreferences/MainActivity.java
+++ SharedPreference/app/src/main/java/br/com/sharedpreferences/MainActivity.java
@@ -1,14 +1,26 @@
 package br.com.sharedpreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText textEditName;
+    AppCompatButton buttonEnviar;
+    TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textEditName = findViewById(R.id.textEditName);
+        buttonEnviar = findViewById(R.id.buttonEnviar);
+        textResult = findViewById(R.id.textResult);
     }
 }
