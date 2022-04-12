8485616fc09e799cb45da05107a4daa1748aab2c
ana204 <anagomes.lucia@hotmail.com>
2022-04-12 17:22:48 -0300

    name file sharedPreferences

--- AppAnotacoes/app/src/main/java/br/com/anotacoes/PreferencesAnnotation.java
+++ AppAnotacoes/app/src/main/java/br/com/anotacoes/PreferencesAnnotation.java
@@ -7,11 +7,13 @@ public class PreferencesAnnotation {
 
     private Context context;
     private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
     private final String FILE_NAME = "anotacion.pref";
 
     public PreferencesAnnotation(Context context){
         this.context = context;
         preferences = context.getSharedPreferences(FILE_NAME, 0);
+        editor = preferences.edit();
     }
 
     public void salvarAnnotation(){
