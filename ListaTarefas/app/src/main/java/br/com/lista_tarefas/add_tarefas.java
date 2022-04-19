6bb27375caddac45c43952f94fbd3037b18d0d3a
ana204 <anagomes.lucia@hotmail.com>
2022-04-19 17:42:21 -0300

    activity add tarefas and layout

---
+++ ListaTarefas/app/src/main/java/br/com/lista_tarefas/add_tarefas.java
@@ -0,0 +1,14 @@
+package br.com.lista_tarefas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_tarefas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_tarefas);
+    }
+}
