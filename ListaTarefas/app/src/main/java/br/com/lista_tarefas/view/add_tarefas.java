c18ed6537328440f377f3ba0b74b10e09375d78d
ana204 <anagomes.lucia@hotmail.com>
2022-04-26 15:50:13 -0300

    item menu

--- ListaTarefas/app/src/main/java/br/com/lista_tarefas/view/add_tarefas.java
+++ ListaTarefas/app/src/main/java/br/com/lista_tarefas/view/add_tarefas.java
@@ -1,8 +1,12 @@
 package br.com.lista_tarefas.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import br.com.lista_tarefas.R;
 
@@ -12,5 +16,24 @@ public class add_tarefas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_tarefas);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.salvar :
+                Toast.makeText(this, "Salvo com sucesso !!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
