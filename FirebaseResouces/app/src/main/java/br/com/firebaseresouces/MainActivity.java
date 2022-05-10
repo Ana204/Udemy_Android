05d8ed0a02e56b090297bf04f1c1baa09541f579
ana204 <anagomes.lucia@hotmail.com>
2022-05-10 14:50:12 -0300

    addValueEventListener

--- FirebaseResouces/app/src/main/java/br/com/firebaseresouces/MainActivity.java
+++ FirebaseResouces/app/src/main/java/br/com/firebaseresouces/MainActivity.java
@@ -1,11 +1,16 @@
 package br.com.firebaseresouces;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,22 +31,46 @@ public class MainActivity extends AppCompatActivity {
     public void addUsers(){
         user = new Users();
 
-        user.setFirstName("Maria");
+        /*user.setFirstName("Maria");
         user.setSecondName("Ferreira");
-        user.setAge(25);
+        user.setAge(25);*/
 
         DatabaseReference users = reference.child("Users");
-        users.child("001").setValue(user);
+       //users.child("001").setValue(user);
+
+        users.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.i("FIREBASEONDATA", "firebase" + snapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     public void addProduct(){
         product = new Product();
 
-        product.setDescription("Nexus");
+       /* product.setDescription("Nexus");
         product.setBrand("Samsung");
-        product.setPrice(999.0);
+        product.setPrice(999.0);*/
 
         DatabaseReference products = reference.child("Product");
-        products.child("003").setValue(product);
+        //products.child("003").setValue(product);
+
+        products.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.i("FirebaseProduct", "PRODUCT: " + snapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
