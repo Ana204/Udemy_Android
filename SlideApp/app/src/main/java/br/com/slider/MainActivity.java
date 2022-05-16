ec22b0ef5de219d20bfe4b797bfa689a0110a818
ana204 <anagomes.lucia@hotmail.com>
2022-05-16 17:37:10 -0300

    Standard slide (SimpleSlide)

--- SlideApp/app/src/main/java/br/com/slider/MainActivity.java
+++ SlideApp/app/src/main/java/br/com/slider/MainActivity.java
@@ -12,11 +12,37 @@ public class MainActivity extends IntroActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
 
         addSlide(new SimpleSlide.Builder()
                 .title("Titulo")
                 .description("Descricao")
+                .image(R.drawable.um)
+                .background(android.R.color.holo_orange_light)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo2")
+                .description("Descricao2")
+                .image(R.drawable.dois)
+                .background(android.R.color.holo_orange_light)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo3")
+                .description("Descricao3")
+                .image(R.drawable.tres)
+                .background(android.R.color.holo_orange_light)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo4")
+                .description("Descricao4")
+                .image(R.drawable.quatro)
+                .background(android.R.color.holo_orange_light)
                 .build());
     }
 }
